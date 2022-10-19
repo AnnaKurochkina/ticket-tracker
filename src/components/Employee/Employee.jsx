b09ea0a9d2665c7d2614bf80b343bfede71ec5f2
Anna Kurochkina <basia220586@gmail.com>
2022-10-19 13:09:00 +0100

    Add onClick event handler

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,15 +1,21 @@
+import {React, useState} from "react";
 import "./Employee.scss";
 // import team from "../../data/team.js"
 
 
 const Employee = (props) => {
+
+    const [counter, setCounter] = useState(0);
+
+    const handleIncrement = () => {
+        setCounter(counter + 1);
+    };
     
-    const deleteHandler = () => {
-        console.log("clicked");
-    }
-    const addHandler = () => {
-        console.log("clicked");
-    }
+    const handleDecrement = () => {
+        if (counter > 0){
+            setCounter(counter - 1);
+        }
+    };
 
 return <div className='card'>
             <div>
@@ -17,9 +23,9 @@ return <div className='card'>
                 <div>{props.employee.role}</div>
             </div>
             <div className="actions">
-                <button className="btn" onClick={deleteHandler}>Delete</button>
-                <span>0</span>
-                <button className="btn" onClick={addHandler}>Add</button>
+                <button className="btn" onClick={handleDecrement}>Delete</button>
+                <span>{counter}</span>
+                <button className="btn" onClick={handleIncrement}>Add</button>
             </div>
         </div>
 }
